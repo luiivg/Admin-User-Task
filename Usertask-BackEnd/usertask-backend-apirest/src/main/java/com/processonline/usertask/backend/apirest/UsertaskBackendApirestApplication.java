2c00591e3e464d46ad00262461a329ca256837d5
Luisa Vargas <luisavg9410@gmail.com>
2018-11-30 02:10:00 -0500

    datasource config

---
+++ Usertask-BackEnd/usertask-backend-apirest/src/main/java/com/processonline/usertask/backend/apirest/UsertaskBackendApirestApplication.java
@@ -0,0 +1,12 @@
+package com.processonline.usertask.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsertaskBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsertaskBackendApirestApplication.class, args);
+	}
+}
